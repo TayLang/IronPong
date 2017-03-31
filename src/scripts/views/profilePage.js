e796d73ecb79815aeaab3ce455102f7b2fbe4eb4
james sewell <jamesjosephsewell@gmail.com>
2017-03-31 14:43:48 -0500

    added an avatar component and have started blocking out the profile page

--- src/scripts/views/profilePage.js
+++ src/scripts/views/profilePage.js
@@ -4,9 +4,48 @@ import STORE from '../store.js'
 
 var ProfilePage = React.createClass({
 
+	componentWillMount: function(){
+
+		//fetch the user info collection
+		this.setState(STORE.data)
+
+	},
+
+	getInitialState: function(){
+
+		return STORE.data
+
+	},
+
 	render: function(){
 
-		return(<div className = 'profile-page-wrapper'>testing profile page</div>)
+		return(
+
+			<div className = 'profile-page-wrapper'>
+
+				<Stats />	
+
+			</div>
+
+		)
+
+	}
+
+})
+
+var Stats = React.createClass({
+
+	render: function(){
+
+		return(
+
+			<div className = 'stats-wrapper'>
+
+
+
+			</div>
+
+		)
 
 	}
 
