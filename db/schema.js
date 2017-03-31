6591cae2636a4824602600ddbf28eab44fee75a2
Denis Bozzato <denis.bozzato@gmail.com>
2017-03-31 14:04:53 -0500

    mc

--- db/schema.js
+++ db/schema.js
@@ -4,12 +4,8 @@ const mongoose = require('mongoose');
 // USERS
 // ----------------------
 const usersSchema = new mongoose.Schema({
-  // required for authentication: DO NOT TOUCH Or You May Get Punched
   email:     { type: String, required: true },
   password:  { type: String, required: true },
-  // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-  
-   // example of optional fields
   name:      { type: String },
   createdAt: { type: Date, default: Date.now }
 
@@ -17,4 +13,5 @@ const usersSchema = new mongoose.Schema({
 
 module.exports = {
   User: mongoose.model('User', usersSchema)
+  
 }
