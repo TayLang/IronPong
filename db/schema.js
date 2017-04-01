bd04e28bd16f6f84b60121c9d4a80c404d3bf592
Denis Bozzato <denis.bozzato@gmail.com>
2017-03-31 19:44:09 -0500

    Included API routes to get the queue, add a player to the queue and delete a player from the queue

--- db/schema.js
+++ db/schema.js
@@ -10,17 +10,17 @@ const usersSchema = new mongoose.Schema({
   
    // example of optional fields
   nickName: {type: String, required: true},
-  createdAt: { type: Date, default: Date.now },
-  wins: {type: Number},
-  losses: {type: Number},
-  winStreak: {type: Number, min: 5},
-  winRatio: {type: Number},
-  totalGames: {type: Number},
-  avatarURL: {type: String},
-  catchPhrase: {type: String},
-  paddleGripStyle: {type: String},
-  signatureMove: {type: String},
-  homeTown: {type: String},
+  // createdAt: { type: Date, default: Date.now },
+  // wins: {type: Number},
+  // losses: {type: Number},
+  // winStreak: {type: Number, min: 5},
+  // winRatio: {type: Number},
+  // totalGames: {type: Number},
+  // avatarURL: {type: String},
+  // catchPhrase: {type: String},
+  // paddleGripStyle: {type: String},
+  // signatureMove: {type: String},
+  // homeTown: {type: String}
 
 
 })
@@ -36,11 +36,10 @@ const gameSchema = new mongoose.Schema({
 })
 
 const queueSchema = new mongoose.Schema({
-
-	members: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-	}]
+	queueMembers: [{
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User'
+  }]
 })
 
 module.exports = {
