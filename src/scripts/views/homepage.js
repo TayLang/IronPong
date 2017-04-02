2a0d1fc6a3134a237622c3f5c74e4975567fb5f2
Reggie Evans <REvans911@gmail.com>
2017-04-02 16:26:50 -0500

    built out home page

--- src/scripts/views/homepage.js
+++ src/scripts/views/homepage.js
@@ -7,21 +7,30 @@ import Header from './components/header'
 var HomePage = React.createClass({
 
 	render: function(){
-
 		return(
-
 			<div className = 'home-page-wrapper'>
 				<Header />
 				<NavBar />
-				<h2>this is a title</h2>
-				<h3>this is a subtitle</h3>
-				<h4>this is a heading</h4>
-				<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-				
+				<div id='home-page-columns'>
+					<div id='button-col'>
+						<a href='#create_game'>
+							<button id='home-button'>Create a Game</button> 
+						</a>
+							<button id='home-button'>Join the Queue</button>
+						<a href='#leaderboard'>
+							<button id='home-button'>View Leaderboard</button>
+						</a>
+						<a href='#profile/:id'>
+							<button id='home-button'>View my Profile</button>
+						</a>
+							<button id='home-button'>View All Players</button>
+					</div>
+					<div id='queue-col'>
+						<h4>Current Queue</h4>
+					</div>
+				</div>
 			</div>
-
 		)
-
 	}
 
 })
